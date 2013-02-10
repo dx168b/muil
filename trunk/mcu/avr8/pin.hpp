fbfe0822c0ce33c9dd5803c706076103fb53b96a
denis.artyomov@gmail.com <denis.artyomov@gmail.com@392897ec-84a6-916e-fd95-d0269ca2dad6>
2013-02-10 10:44:07 +0000

    * Added some functionality into pin.hpp for AVR

--- trunk/mcu/avr8/pin.hpp
+++ trunk/mcu/avr8/pin.hpp
@@ -1,6 +1,34 @@
 #ifndef PIN_HPP_FILE_INCLUDED_
 #define PIN_HPP_FILE_INCLUDED_
 
+/* Examples:
+
+  // Declare InpPin as 0th pin of port B:
+  typedef Pin<'B', 0> InpPin;
+
+  // Declare LED1Pin as inverted first pin of port B:
+  typedef Pin<'B', 1, 'L'> LED1Pin;
+
+  // Declare LED2Pin as inverted second pin of port B:
+  typedef Pin<'B', 2> LED2Pin;
+
+  // Configure InpPin as input with pulled-up resistor
+  InpPin::ConfInPulledUp();
+
+  // Configure LED1Pin and LED2Pin as output pins
+  LED1Pin::ConfOut();
+  LED2Pin::ConfOut();
+
+  // Read pin state from InpPin
+  bool pin_state = InpPin::Signaled();
+
+  // Turn on LED1Pin
+  LED1Pin::On();
+
+  // Turn off LED2Pin
+  LED1Pin::Off();
+*/
+
 template<char port, int pin_no, char active_state = 'H'> class Pin {};
 template<char port, int pin_no> class PinBase {};
 
@@ -11,6 +39,7 @@ class PinBase<LETTER, pin_no>                                     \
 public:                                                           \
 	enum { mask = 1 << pin_no };                                  \
 	static void ConfIn() { DIR &= ~mask; }                        \
+	static void ConfInPulledUp() { DIR &= ~mask; OUT |= mask; }   \
 	static void ConfOut() { DIR |= mask; }                        \
 	static void Cpl() { OUT ^= mask; }                            \
 };                                                                \
@@ -21,6 +50,7 @@ public:                                                           \
 	static void On() { OUT |= mask; }                             \
 	static void Off() { OUT &= ~mask; }                           \
 	static bool Signalled() { return IN & mask; }                 \
+	static bool Latched() { return OUT & mask; }                  \
 };                                                                \
 template<int pin_no>                                              \
 class Pin<LETTER, pin_no, 'L'> : public PinBase<LETTER, pin_no> { \
@@ -29,6 +59,7 @@ public:                                                           \
 	static void On() { OUT &= ~mask; }                            \
 	static void Off() { OUT |= mask; }                            \
 	static bool Signalled() { return !(IN & mask); }              \
+	static bool Latched() { return !(OUT & mask); }               \
 };
 
 #ifdef PORTA
